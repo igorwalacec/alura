b37cb3e5d32350be47dca3e367e8a0b473702ded
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2021-07-13 01:49:01 -0300

    Tasks com retorno

--- ParalelismoMundoReal/ByteBank/ByteBank.View/MainWindow.xaml.cs
+++ ParalelismoMundoReal/ByteBank/ByteBank.View/MainWindow.xaml.cs
@@ -39,35 +39,43 @@ namespace ByteBank.View
             BtnProcessar.IsEnabled = false;
 
             var contas = r_Repositorio.ObterContaClientes();
-
-            var resultado = new List<string>();
+            
 
             AtualizarView(new List<string>(), TimeSpan.Zero);
 
             var inicio = DateTime.Now;
-           
-            var contasTarefas = contas.Select(conta =>
-            {
-                return Task.Factory.StartNew(() =>
-                {
-                    var resultadoConta = r_Servico.ConsolidarMovimentacao(conta);
-                    resultado.Add(resultadoConta);
-                });
-            }).ToArray();
-
+            
             //Task.WaitAll(contasTarefas);
-            TaskScheduler.FromCurrentSynchronizationContext();
+            
 
-            Task.WhenAll(contasTarefas)
+            ConsolidarContas(contas)
                 .ContinueWith((task) => {
                     var fim = DateTime.Now;
+                    var resultado = task.Result;
                     AtualizarView(resultado, fim - inicio);
                 }, taskSchedulerUI)
                 .ContinueWith(task => {
                     BtnProcessar.IsEnabled = true;
                 }, taskSchedulerUI);
         }
+        private Task<List<string>> ConsolidarContas(IEnumerable<ContaCliente> contas)
+        {
+            var resultado = new List<string>();
 
+            var tasks = contas.Select(conta =>
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    var contaResultado = r_Servico.ConsolidarMovimentacao(conta);
+                    resultado.Add(contaResultado);
+                });
+            });
+
+            return Task.WhenAll(tasks)
+                .ContinueWith(t => {
+                    return resultado;
+                });
+        }
         private void AtualizarView(List<String> result, TimeSpan elapsedTime)
         {
             var tempoDecorrido = $"{ elapsedTime.Seconds }.{ elapsedTime.Milliseconds} segundos!";
